a9f034b1729602a07c277cc85684de1c690ba493
jesusmaclop <jmacias@itexico.net>
2018-12-12 16:28:50 -0600

    first test

---
+++ build/precache-manifest.cb5f5503b47d839a85713b9f8837d7c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "11f994c421f2e065dd03",
+    "url": "/static/js/main.11f994c4.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "11f994c421f2e065dd03",
+    "url": "/static/css/main.56bb3870.chunk.css"
+  },
+  {
+    "revision": "043fa9c922f008d6f51de5e5ea4ef53e",
+    "url": "/index.html"
+  }
+];
